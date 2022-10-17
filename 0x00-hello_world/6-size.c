e499abbe0f850a65765aac6e011b0e9762367ff2
iamceezee <usenekem@gmail.com>
2022-10-16 17:28:09 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- is the entry point
+ * for the program
+ *
+ * Return zero is no errors
+ * and non-zero is errors
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
